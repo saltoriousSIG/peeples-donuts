a4e11d443c18cf1e26077f09131a6ab2cc7953a1
saltoriousSIG <cryptobet95@gmail.com>
2025-11-28 18:58:40 -0500

    ok

--- hooks/useCanBuyGlaze.ts
+++ hooks/useCanBuyGlaze.ts
@@ -9,7 +9,7 @@ import { Strategy } from "@/types/pool.type";
 export const STRATEGY_MINUTES_BREAKEVEN = {
   [Strategy.CONSERVATIVE]: 30,
   [Strategy.MODERATE]: 60,
-  [Strategy.AGGRESSIVE]: 160,
+  [Strategy.AGGRESSIVE]: 130,
   [Strategy.DEGEN]: 200,
 }
 
