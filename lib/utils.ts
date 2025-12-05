4c1d1158e82cf81ca2d80d16f037f006802763de
saltoriousSIG <cryptobet95@gmail.com>
2025-12-05 01:50:56 -0500

    ok

--- lib/utils.ts
+++ lib/utils.ts
@@ -53,7 +53,7 @@ export async function getEthPrice(): Promise<number> {
 export const STRATEGY_MINUTES_BREAKEVEN:Record<Strategy, number> = {
   [Strategy.CONSERVATIVE]: 30,
   [Strategy.MODERATE]: 60,
-  [Strategy.AGGRESSIVE]: 170,
+  [Strategy.AGGRESSIVE]: 160,
   [Strategy.DEGEN]: 200,
 }
 
