e50c0f021c7045a4c61482bbe17fab087accb1e0
saltoriousSIG <cryptobet95@gmail.com>
2025-12-09 08:50:51 -0500

    ok

---
+++ app/api/webhooks/ping.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  if (
+    req.headers.get("authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  try {
+    return NextResponse.json({
+      success: true,
+      message: "pong",
+    });
+  } catch (e: any) {
+    return NextResponse.json(
+      { error: e.message || "Unknown error" },
+      { status: 500 }
+    );
+  }
+}
